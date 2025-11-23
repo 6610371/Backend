f1a8ffb8425e02b01a9e6137b0e2216d30350d0d
6610371 <sunchhay.s66@rsu.ac.th>
2025-11-23 17:38:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -300,8 +300,9 @@ app.delete('/cards/:cardId', (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port 3000`);
-});
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log(`Server running on port 3000`);
+// });
+
 
 module.exports = app;
